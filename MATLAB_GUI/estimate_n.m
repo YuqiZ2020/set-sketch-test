817590d0aeb04bfe5ee5e4cb5d0b06222731be11
Wenxuan <wenxuanlu@hotmail.com>
2022-12-09 18:56:47 -0500

    MATLAB GUI

--- MATLAB_GUI/estimate_n.m
+++
@@ -1,9 +0,0 @@
-function [Xm_inv] = estimate_n(registers, a, b, m)
-%ESTIMATE_N Estimate the cardinality from the registers
-Xm = 0;
-for i = 1:length(registers)
-    Xm = Xm + a * b^(-registers(i));
-end
-Xm = log(b) / (1 - 1/b) / m * Xm;
-Xm_inv = 1 / Xm;
-end
