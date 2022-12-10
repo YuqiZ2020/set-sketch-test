eba2fd965e40df5d76e0964418d72628a1a50882
Trisha Karani <trisha.karani@gmail.com>
2022-12-09 22:51:47 -0500

    testing parameters

---
+++ parameter_testing.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+printf "parameter testing\n" > file.txt
+
+counter = 1
+for m in 1 10 20 40; do
+    for b in  1.5 1.2 1.01; do 
+        for a in 0.1 5 10 20; do
+            for q in 0.1 10 20; do 
+                for k in 1 10 15 20 30; do 
+		    start=$SECONDS
+		    ./benchmark-param.out test/ output_fish/output_${counter}.txt $m $b $a $q $k  
+		    duration=$(( SECONDS - start ))
+		    printf "%d %d %f %f %f %d %d\n" $counter $m $b $a $q $k $duration >> file.txt
+		    counter=$[$counter +1]
+		    #counter=$((counter+1))
+done
+done
+done
+done
+done
+
